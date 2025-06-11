d456f595a7a36974c832c3bed61b0a1c41e4e9e0
Leo Lu <lyuholu@gmail.com>
2025-06-11 18:28:53 +0800

    feat: add code state

--- client/app/rooms/[id]/page.tsx
+++ client/app/rooms/[id]/page.tsx
@@ -1,22 +1,29 @@
 'use client';
 
+import { useState } from 'react';
 import { useParams } from 'next/navigation';
 import { Editor } from '@monaco-editor/react';
 
 export default function Room() {
     const { id } = useParams();
+    const [code, setCode] = useState<string>(
+        "print('Hello!')\nprint('My name is Elder Price.')"
+    );
+
+    function handleCodeChange(value: string | undefined) {
+        setCode(value ?? '');
+    }
 
     return (
         <>
-            <div className="fixed left-0 top-0 w-1/2 h-screen bg-[#1e1e1e]">
+            <div className="fixed right-0 top-0 w-1/2 h-screen bg-[#1e1e1e]">
                 <Editor
                     theme="vs-dark"
                     width="100%"
                     height="100%"
                     defaultLanguage="python"
-                    defaultValue={
-                        "print('Hello!')\nprint('My name is Elder Price.')"
-                    }
+                    value={code}
+                    onChange={handleCodeChange}
                     options={{
                         minimap: {
                             enabled: false,
