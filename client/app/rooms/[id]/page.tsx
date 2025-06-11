4446e8d725fb2a91054357690b42c5a080062d6c
Leo Lu <lyuholu@gmail.com>
2025-06-11 18:14:14 +0800

    style: set the size/position of the editor

--- client/app/rooms/[id]/page.tsx
+++ client/app/rooms/[id]/page.tsx
@@ -8,11 +8,22 @@ export default function Room() {
 
     return (
         <>
-            <Editor
-                height="90vh"
-                defaultLanguage="javascript"
-                defaultValue={"print('hello')"}
-            />
+            <div className="fixed left-0 top-0 w-1/2 h-screen bg-[#1e1e1e]">
+                <Editor
+                    theme="vs-dark"
+                    width="100%"
+                    height="100%"
+                    defaultLanguage="python"
+                    defaultValue={
+                        "print('Hello!')\nprint('My name is Elder Price.')"
+                    }
+                    options={{
+                        minimap: {
+                            enabled: false,
+                        },
+                    }}
+                />
+            </div>
         </>
     );
 }
