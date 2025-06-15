73f0f074fc3ec39605316779e46f0c7dd3196da3
Leo Lu <lyuholu@gmail.com>
2025-06-15 19:28:46 +0800

    feat: add "Find Your Interview Buddy!" button

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-    return <></>;
+    return (
+        <>
+            <main className="fixed left-0 top-0 w-full h-screen flex flex-col justify-center items-center">
+                <button className="bg-emerald-500 p-4 font-bold text-white rounded-xl hover:bg-emerald-600">
+                    Find Your Interview Buddy!
+                </button>
+            </main>
+        </>
+    );
 }
